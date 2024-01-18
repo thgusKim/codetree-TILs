e54f86a8bd6705c9bac73fccebde363c5d531d27
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2024-01-18 18:55:44 +0900

    Title: 알파벳에 따른 평가, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240118/알파벳에 따른 평가/evaluation-by-alphabet.java
+++ 240118/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
+        char a = sc.next().charAt(0);
 
-        if(a=="S"){
+        if(a=='S'){
             System.out.println("Superior");
-        }else if(a=="A"){
+        }else if(a=='A'){
             System.out.println("Excellent");
-        }else if(a=="B"){
+        }else if(a=='B'){
             System.out.println("Good");
-        }else if(a=="C"){
+        }else if(a=='C'){
             System.out.println("Usually");
-        }else if(a=="D"){
+        }else if(a=='D'){
             System.out.println("Effort");
         }else{
             System.out.println("Failure");
