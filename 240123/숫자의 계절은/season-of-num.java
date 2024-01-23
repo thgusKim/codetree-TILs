123f0a24c220fbbdd2cd8b64b045fe80cc47375f
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2024-01-23 18:33:57 +0900

    Title: 숫자의 계절은, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240123/숫자의 계절은/season-of-num.java
+++ 240123/숫자의 계절은/season-of-num.java
@@ -11,7 +11,7 @@ public class Main{
             System.out.println("Spring");
         }else if(m < 9){
             System.out.println("Summer");
-        }else if(m < 12){
+        }else{
             System.out.println("Fall");
         }
     }
