3795f1a5444b158816e8070350f0520217812ca9
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2024-01-24 18:18:50 +0900

    Title: 좀 더 어려운 수학 점수, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240124/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int aMath = sc.nextInt();
+        int aEng = sc.nextInt();
+        int bMath = sc.nextInt();
+        int bEng = sc.nextInt();
+
+        if(aMath==bMath){
+            if(aEng > bEng){
+                System.out.println("A");
+            }else{
+                System.out.println("B");
+            }
+        }else{
+            if(aMath > bMath){
+                System.out.println("A");
+            }else{
+                System.out.println("B");
+            }
+        }
+    }
+}
