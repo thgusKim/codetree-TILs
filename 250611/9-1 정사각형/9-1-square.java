e2dd208ea25a0f3e35b278aaf2eb4432ed69213d
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2025-06-11 10:53:09 +0900

    Title: 9-1 정사각형, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250611/9-1 정사각형/9-1-square.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 9;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(cnt--);
+                if(cnt==0) cnt=9;
+            }
+            System.out.println();
+        }
+    }
+}
