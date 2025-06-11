a2a23e649d51ad51d41df672d843caa5d2b0affe
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2025-06-11 11:18:40 +0900

    Title: 좌우 지그재그 출력, Time: 193ms, Memory: 12MB, Status: Passed - Codetree

--- 250611/좌우 지그재그 출력/print-lr-zigzag.java
+++ 250611/좌우 지그재그 출력/print-lr-zigzag.java
@@ -4,18 +4,13 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int cnt = 1;
-
+        
         for(int i=0; i<n; i++){
-            if(i%2==0){
-                cnt = (i*n)+1;
-                for(int j=0; j<n; j++){
-                    System.out.print(cnt++ + " ");
-                }
-            }else{
-                cnt = (i+1)*n;
-                for(int j=0; j<n; j++){
-                    System.out.print(cnt-- + " ");
+            for(int j=0; j<n; j++){
+                if(i%2==0){
+                    System.out.print((n*i)+1+j + " ");
+                }else{
+                    System.out.print((i+1)*n-j + " ");
                 }
             }
             System.out.println();
