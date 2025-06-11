bcc9378cdc35df05b54a41711cdbd2ed83ec1cbc
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2025-06-11 13:55:56 +0900

    Title: 모양대로 수 출력하기, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

--- 250611/모양대로 수 출력하기/print-out-numbers-in-specific-shape.java
+++ 250611/모양대로 수 출력하기/print-out-numbers-in-specific-shape.java
@@ -5,12 +5,12 @@ public class Main{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(i>j){
+        for(int i=n; i>=1; i--){
+            for(int j=n; j>=1; j--){
+                if(j>i){
                     System.out.print("  ");
                 }else{
-                    System.out.print(n-j+" ");
+                    System.out.print(j+" ");
                 }
             }
             System.out.println();
