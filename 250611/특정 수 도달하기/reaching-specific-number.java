4e5f925db103bec99973276d4ac0b6caf90bb1a8
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2025-06-11 17:01:42 +0900

    Title: 특정 수 도달하기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250611/특정 수 도달하기/reaching-specific-number.java
+++ 250611/특정 수 도달하기/reaching-specific-number.java
@@ -8,13 +8,17 @@ public class Main{
 
         for(int i=0; i<10; i++){
             arr[i] = sc.nextInt();
-            if(arr[i] >= 250 || i==9) {
+            if(arr[i] >= 250) {
                 System.out.print(sum+" "+(float)sum/i);
                 break;
             }else{
                 sum += arr[i]; 
             }
 
+            if(i==9) {
+                System.out.print(sum+" "+(float)sum/10);
+                break;
+            }
         }
     }
 }
