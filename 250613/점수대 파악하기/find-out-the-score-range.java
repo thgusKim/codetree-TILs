88d0a4061dd6defb10b91ad416117b104aa6d935
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2025-06-13 10:25:01 +0900

    Title: 점수대 파악하기, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250613/점수대 파악하기/find-out-the-score-range.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[100];
+        int[] count_arr = new int[11];
+        int cnt = 0;
+
+        for(int i=0; i<100; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] == 0){
+                cnt = i;
+                break;
+            }
+
+            if(arr[i] == 100){
+                arr[i] = 10;
+            }else{
+                arr[i] = arr[i]/10%10;
+            }
+        }
+
+        for(int i=0; i<cnt; i++){
+            count_arr[arr[i]]++;
+        }
+
+        for(int i=10; i>0; i--){
+            System.out.println(i*10 + " - " + count_arr[i]);
+        }
+    }
+}
