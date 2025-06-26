87956576598a5596d1e9a8aa54aef91dace4d4fb
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2025-06-26 11:18:09 +0900

    Title: 아래로 사각형 채우기, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

--- 250626/아래로 사각형 채우기/filling-rectangle-with-downflow.java
+++ 250626/아래로 사각형 채우기/filling-rectangle-with-downflow.java
@@ -9,7 +9,7 @@ public class Main{
 
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                System.out.print(i+1+(j*5) + " ");
+                System.out.print(i+1+(j*n) + " ");
             }
             System.out.println();
         }
