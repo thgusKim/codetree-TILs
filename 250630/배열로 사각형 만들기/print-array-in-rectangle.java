9482fefbc966eac20cb1425955a0b271d84d7f37
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2025-06-30 15:10:30 +0900

    Title: 배열로 사각형 만들기, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

--- 250630/배열로 사각형 만들기/print-array-in-rectangle.java
+++ 250630/배열로 사각형 만들기/print-array-in-rectangle.java
@@ -7,13 +7,9 @@ public class Main{
             for(int j=0; j<n; j++){
                 if(i==0 || j==0){
                     arr[i][j] = 1;
+                }else{
+                    arr[i][j] = arr[i-1][j] + arr[i][j-1];
                 }
-            }
-        }
-
-        for(int i=1; i<n; i++){
-            for(int j=1; j<n; j++){
-                arr[i][j] = arr[i-1][j] + arr[i][j-1];
             }
         }
 
