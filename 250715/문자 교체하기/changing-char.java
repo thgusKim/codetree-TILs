193f40a085f153d83c1abcfe922db94edd8c70cb
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2025-07-15 10:46:25 +0900

    Title: 문자 교체하기, Time: 114ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250715/문자 교체하기/changing-char.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.next();
+        String s2 = sc.next();
+
+        s2 = s1.substring(0,2) + s2.substring(2,s2.length());
+        System.out.println(s2);
+    }
+}
