fa587bdc6fb408c0d5c1b5ad65500235f374f405
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2025-07-15 17:09:13 +0900

    Title: 문자열 계속 지우기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250715/문자열 계속 지우기/keep-removing-string.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        int aLen = a.length();
+        int bLen = b.length();
+
+        for(int i=0; i<aLen-bLen+1; i++){
+
+            if(a.substring(i, i+bLen).equals(b)){
+                a = a.substring(0,i) + a.substring(i+bLen);
+                i=0;
+            }
+            aLen = a.length();
+        }
+
+    }
+}
