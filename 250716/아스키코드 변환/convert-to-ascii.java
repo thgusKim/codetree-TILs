73f135ef47653f4e0b53a4df3bde646fb94b83b6
SohyeonKim <74132692+thgusKim@users.noreply.github.com>
2025-07-16 13:12:52 +0900

    Title: 아스키코드 변환, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250716/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = (int)sc.next().charAt(0);
+        char b = (char)sc.nextInt();
+
+        System.out.println(a + " " + b);
+    }
+}
